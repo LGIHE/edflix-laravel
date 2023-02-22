61f7c74ef8e67c0d241ca84d4cb2fde567e2c889
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-02-22 16:16:18 +0300

    remove RTL

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -65,15 +65,6 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark {{ $activePage == 'rtl' ? ' active bg-gradient-primary' : '' }}  "
-                    href="{{ route('rtl') }}">
-                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
-                    </div>
-                    <span class="nav-link-text ms-1">RTL</span>
-                </a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link text-dark {{ $activePage == 'notifications' ? ' active bg-gradient-primary' : '' }}  "
                     href="{{ route('notifications') }}">
                     <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
