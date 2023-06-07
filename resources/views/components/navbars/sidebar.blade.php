84fd80dbfc0d046f1288ccc7a6f45104c79ccab4
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-07 16:06:39 +0300

    Add reports menu item

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -2,13 +2,18 @@
     .navbar-vertical.navbar-expand-xs .navbar-collapse {
         overflow: visible!important;
     }
+
+    /* #logo-text {
+        font-weight: 800;
+        font-size:
+    } */
 </style>
 
 @props(['activePage'])
 
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-transparent" id="sidenav-main">
     <div class="sidenav-header">
-        <img src="{{ asset('assets') }}/img/logos/edflix-logo.png" class="cursor-pointer h-80 w-80" id="iconSidenav" alt="">
+        <img src="{{ asset('assets') }}/img/logos/EDPLAN.png" class="cursor-pointer h-80 w-80" id="iconSidenav" alt="">
     </div>
     <hr class="horizontal light mt-0 mb-2">
     <div class="collapse navbar-collapse w-auto max-height-vh-100" id="sidenav-collapse-main">
@@ -61,6 +66,15 @@
                         <i class="material-icons opacity-10">book</i>
                     </div>
                     <span class="nav-link-text text-m text-bold ms-1" style="font-size:1rem;">Subjects</span>
+                </a>
+            </li>
+            <li class="nav-item mt-3">
+                <a class="nav-link text-dark {{ $activePage == 'subjects' ? ' active bg-gradient-info' : '' }} "
+                    href="#">
+                    <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">folder</i>
+                    </div>
+                    <span class="nav-link-text text-m text-bold ms-1" style="font-size:1rem;">Reports</span>
                 </a>
             </li>
             @endif
