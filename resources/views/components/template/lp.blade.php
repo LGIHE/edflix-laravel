2efdc30be0190690d963cb2a065d1d00cfc5692d
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-22 13:03:59 +0300

    Merge annex and steps into main LP download file

--- resources/views/components/template/lp.blade.php
+++ resources/views/components/template/lp.blade.php
@@ -251,25 +251,33 @@
             @endforeach
         </table>
     </div>
+
+    {{-- START ANNEX --}}
+    <div class="m-4">
+        <h2><strong>Annexes</strong></h2>
+        @foreach ($annexes as $annex)
+            @if (Str::endsWith($annex->annex_file, ['.jpg', '.jpeg', '.png']))
+                <table class="mt-5 page-break" style="border-collapse: collapse; margin-left:20pt" cellspacing="0">
+
+                    <tr style="height: 33pt">
+                        <td class="column" style="width: 150pt;">
+                            <p class="s2 column-head" style="font-size:20px;"><span style="font-weight:bold;">Annex:</span>
+                                {{ $annex->title }}</p>
+                        </td>
+                    </tr>
+                    <tr style="height: 33pt">
+                        <td class="column" style="width: 150pt;">
+                            <img src="{{ url('/annex/' . $annex->annex_file) }}" alt="{{ $annex->title }}" width="1000"
+                                height="600">
+                        </td>
+                    </tr>
+
+                </table>
+                <br>
+            @endif
+        @endforeach
+    </div>
+
 </body>
 
 </html>
-
-@extends('components.template.annex', [
-    'annexes' => $annexes,
-    'lesson' => $lesson,
-    'subject' => $subject,
-    'school' => $school,
-    'owner' => $owner,
-    'duration' => $duration,
-])
-
-{{-- @extends('components.template.step', [
-    'steps' => $steps,
-    'lesson' => $lesson,
-    'subject' => $subject,
-    'school' => $school,
-    'owner' => $owner,
-    'duration' => $duration,
-    'activity' => $lesson->activity_aim
-]) --}}
