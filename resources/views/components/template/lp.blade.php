b092a64e5328779d68e9cc17b05e95142c051ca3
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-06-22 12:58:31 +0300

    Update the steps page

--- resources/views/components/template/lp.blade.php
+++ resources/views/components/template/lp.blade.php
@@ -136,6 +136,121 @@
             </div>
         </div>
     </div>
+
+    {{-- START OF STEPS --}}
+    <div class="m-4 page-break">
+        <table class="mt-4" style="border-collapse: collapse; border: none;margin-left:20pt" cellspacing="0" cellpadding="0">
+            <tr style="border: none;height: 33pt">
+                <td class="column" style="border: none;width:300pt;">
+                    <img class="logo" src="{{ asset('assets') }}/img/logos/EDPLAN.png" width="250" height="75"
+                    alt="EDFLIX">
+                </td>
+                <td class="column" style="border: none;width: 300pt;">
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>School: </strong>{{ $school->name }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Class: </strong>{{ $lesson->class }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Theme: </strong>{{ $lesson->theme }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Number of learners: </strong>{{ $lesson->learners_no }}
+                        </div>
+                    </div>
+                </td>
+                <td class="column" style="border: none;width: 300pt;">
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Teacher: </strong>{{ $owner->name }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Subject: </strong>{{ $subject->name }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Topic: </strong>{{ $lesson->topic }}
+                        </div>
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <strong>Duration:
+                            </strong>{{ Carbon\CarbonInterval::minutes($duration)->cascade()->forHumans() ?? '' }}
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <div class="row align-items-start m-4">
+            <div class="col">
+                <strong>Activity Aim:</strong> {{ $lesson->activity_aim }}
+            </div>
+        </div>
+
+        <h2 class="lp-heading mt-3" style="font-family: Arial, Helvetica, sans-serif;">Steps</h2>
+
+        <table class="mt-4" style="border-collapse: collapse; margin-left:20pt" cellspacing="0">
+            <tr style="height: 33pt">
+                <td class="column" style="width:75pt;">
+                    <p class="s2 column-head">Duration of activity</p>
+                </td>
+                <td class="column" style="width: 150pt;">
+                    <p class="s2 column-head">Teacher’s activity</p>
+                </td>
+                <td class="column" style="width: 113pt;">
+                    <p class="s2 column-head">Students’ activity</p>
+                </td>
+                <td class="column" style="width: 111pt;">
+                    <p class="s2 column-head">Knowledge, value and skills</p>
+                </td>
+                <td class="column" style="width: 149pt;">
+                    <p class="s2 column-head">Output:</p>
+                </td>
+                <td class="column" style="width: 149pt;">
+                    <p class="s2 column-head">Assessment criteria</p>
+                </td>
+            </tr>
+            @foreach ($steps as $step)
+            <tr style="height: 33pt">
+                <td class="column" style="width:75pt;">
+                    <p class="s2 column-head">Step
+                        {{ $step->step }}<br>{{ Carbon\CarbonInterval::minutes($step->duration)->cascade()->forHumans() ?? '' }}
+                    </p>
+                </td>
+                <td class="column" style="width: 150pt;">
+                    <p class="s2 column-head">{{ $step->teacher_activity }}</p>
+                </td>
+                <td class="column" style="width: 113pt;">
+                    <p class="s2 column-head">{{ $step->student_activity }}</p>
+                </td>
+                <td class="column" style="width: 111pt;">
+                    <p class="s2 column-head">
+                        <strong>Knowledge: </strong>{{ $step->knowledge }}<br>
+                        <strong>Values: </strong>{{ $step->values }}<br>
+                        <strong>Skills: </strong>{{ $step->skills }}
+                    </p>
+                </td>
+                <td class="column" style="width: 149pt;">
+                    <p class="s2 column-head">{{ $step->output }}</p>
+                </td>
+                <td class="column" style="width: 149pt;">
+                    <p class="s2 column-head">{{ $step->assessment_criteria }}</p>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 
 </html>
@@ -149,7 +264,7 @@
     'duration' => $duration,
 ])
 
-@extends('components.template.step', [
+{{-- @extends('components.template.step', [
     'steps' => $steps,
     'lesson' => $lesson,
     'subject' => $subject,
@@ -157,4 +272,4 @@
     'owner' => $owner,
     'duration' => $duration,
     'activity' => $lesson->activity_aim
-])
+]) --}}
--- resources/views/components/template/step.blade.php
+++ resources/views/components/template/step.blade.php
@@ -1,7 +1,7 @@
-
+@foreach ($steps as $step)
     <div class="row align-items-start">
         <div class="col col-md-4">
-            <img class="logo" src="{{ asset('assets') }}/img/logos/EDPLAN.png" width="250" height="75"
+            <img class="logo" src="{{ asset('assets') }}/img/logos/edflix-logo.png" width="250" height="75"
                 alt="EDFLIX">
         </div>
         <div class="col col-md-4">
@@ -50,16 +50,9 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row align-items-start mt-4">
-            <div class="col">
-                <strong>Activity Aim:</strong> {{ $activity }}
-            </div>
-        </div>
-    </div>
-    {{-- @if ($step->step == 1) --}}
+    @if ($step->step == 1)
         <h2 class="lp-heading mt-3">Steps</h2>
-    {{-- @endif --}}
+    @endif
 
     <table class="mt-4 page-break" style="border-collapse: collapse; margin-left:20pt" cellspacing="0">
         <tr style="height: 33pt">
@@ -82,7 +75,7 @@
                 <p class="s2 column-head">Assessment criteria</p>
             </td>
         </tr>
-        @foreach ($steps as $step)
+
         <tr style="height: 33pt">
             <td class="column" style="width:75pt;">
                 <p class="s2 column-head">Step
@@ -109,7 +102,6 @@
                 <p class="s2 column-head">{{ $step->assessment_criteria }}</p>
             </td>
         </tr>
-        @endforeach
     </table>
-<br>
-
+    <br>
+@endforeach
