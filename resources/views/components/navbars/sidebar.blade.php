f8104813e84c2dd0d30a3dd1f218361b03afbbac
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-05 14:06:46 +0300

    Make the sidenav logo redirect to dashboard

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -13,7 +13,9 @@
 
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-transparent" id="sidenav-main">
     <div class="sidenav-header">
-        <img src="{{ asset('assets') }}/img/logos/actnow-logo.png" class="cursor-pointer h-80 w-80" id="iconSidenav" alt="">
+        <a href="{{ route('dashboard') }}">
+            <img src="{{ asset('assets') }}/img/logos/actnow-logo.png" class="cursor-pointer h-80 w-80" id="iconSidenav" alt="">
+        </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
     <div class="collapse navbar-collapse w-auto max-height-vh-100" id="sidenav-collapse-main">
