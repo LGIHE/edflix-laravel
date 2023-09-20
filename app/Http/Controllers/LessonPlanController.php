be0df38c78c21762c858a977e707ce3fb4f16a19
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-20 15:32:02 +0300

    Add uploads and creation of LPs using Word documents

--- app/Http/Controllers/LessonPlanController.php
+++ app/Http/Controllers/LessonPlanController.php
@@ -11,6 +11,7 @@ use App\Models\School;
 use App\Models\LessonPlan;
 use App\Models\LessonStep;
 use App\Models\LessonAnnex;
+use Faker\Core\Number;
 use Intervention\Image\Facades\Image;
 use Maatwebsite\Excel\Facades\Excel;
 use Jenssegers\Agent\Facades\Agent;
@@ -18,6 +19,8 @@ use App\Models\Logs;
 use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 use Spipu\Html2Pdf\Html2Pdf;
+use PhpOffice\PhpWord\IOFactory;
+use Illuminate\Support\Facades\Validator;
 
 class LessonPlanController extends Controller
 {
@@ -199,16 +202,213 @@ class LessonPlanController extends Controller
 
     public function uploadLessonPlan()
     {
+        // request()->validate([
+        //     'subject' => 'required',
+        //     'lesson_plan_file' => 'required|mimes:xlsx,xls|max:1024'
+        // ]);
+
+        // $import = new \App\Imports\Lessonplan\getSheets();
+
+        // Excel::import($import, request()->lesson_plan_file);
+
+        // $log['message'] = 'Lessonplan was uploaded.';
+        // $log['user_id'] = Auth()->user()->id;
+        // $log['action'] = 'Upload Lessonplan';
+        // $log['ip_address'] = request()->ip();
+        // $log['platform'] = Agent::platform() . '-' .Agent::version(Agent::platform());
+        // $log['agent'] = Agent::browser() . '-' .Agent::version(Agent::browser());
+
+        // Logs::create($log);
+
+        // return redirect()
+        //     ->route('lesson.plans')
+        //     ->with('status', 'The Lesson Plan has been uploaded successfully.');
+
         request()->validate([
+            'teacher' => 'required',
             'subject' => 'required',
-            'lesson_plan_file' => 'required|mimes:xlsx,xls|max:1024'
+            'lesson_plan_file' => 'required|mimetypes:application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         ]);
 
-        $import = new \App\Imports\Lessonplan\getSheets();
+        // Get the uploaded Word file
+        $uploadedFile = request()->file('lesson_plan_file');
 
-        Excel::import($import, request()->lesson_plan_file);
+        // Load the Word document using phpoffice/phpword
+        $phpWord = IOFactory::load($uploadedFile);
 
-        $log['message'] = 'Lessonplan was uploaded.';
+        // Extract data from the Word document
+        $data = [];
+
+        foreach ($phpWord->getSections() as $section) {
+            foreach ($section->getElements() as $element) {
+                if (get_class($element) == 'PhpOffice\PhpWord\Element\Table') {
+                    $rows = $element->getRows();
+
+                    if (!empty($rows)) {
+                        $tableData = [];
+
+                        foreach ($rows as $row) {
+                            $cells = $row->getCells();
+                            $rowData = [];
+
+                            // dd($cells[0]->getElements()[0]->getElements())['text'];
+
+                            foreach ($cells as $cell) {
+                                $els = $cell->getElements();
+                                $cellText = '';
+
+                                foreach ($els as $e) {
+                                    if (get_class($e) == 'PhpOffice\PhpWord\Element\TextRun') {
+                                        // Handle TextRun elements within the cell
+                                        foreach ($e->getElements() as $textElement) {
+                                            if (get_class($textElement) == 'PhpOffice\PhpWord\Element\Text') {
+                                                $cellText .= $textElement->getText();
+                                            }
+                                        }
+                                    }
+                                }
+
+                                // Add the cell text to rowData array
+                                $rowData[] = $cellText;
+                            }
+
+                            // Add the row data to tableData array
+                            $tableData[] = $rowData;
+                        }
+
+                        // Add the table data to the $data array, with a unique key for each table
+                        $data[] = $tableData;
+                    }
+                }
+            }
+        }
+
+        $response = [];
+
+        foreach ($data as $contents)
+        {
+            if ($contents[0][0] == 'INFO 1')
+            {
+                $info_1_rows = array_slice($contents, 1);
+                $col_1 = $info_1_rows[0];
+                $col_2 = $info_1_rows[1];
+                $col_3 = $info_1_rows[2];
+
+                $info_1['theme'] = $col_1[1];
+                $info_1['topic'] = $col_1[3];
+                $info_1['class'] = $col_2[1];
+                $info_1['term'] = $col_2[3];
+                $info_1['male_learners'] = intval($col_3[1]);
+                $info_1['female_learners'] = intval($col_3[3]);
+            }
+
+            if ($contents[0][0] == 'INFO 2')
+            {
+                $info_2_rows = array_slice($contents, 1);
+
+                $info_2['competency'] = $info_2_rows[0][1];
+                $info_2['learning_outcomes'] = $info_2_rows[1][1];
+                $info_2['generic_skills'] = $info_2_rows[2][1];
+                $info_2['values'] = $info_2_rows[3][1];
+                $info_2['cross_cutting_issues'] = $info_2_rows[4][1];
+                $info_2['key_learning_outcomes'] = $info_2_rows[5][1];
+                $info_2['pre_requisite_knowledge'] = $info_2_rows[6][1];
+                $info_2['learning_materials'] = $info_2_rows[7][1];
+                $info_2['learning_methods'] = $info_2_rows[8][1];
+                $info_2['references'] = $info_2_rows[9][1];
+                $info_2['activity_aim'] = $info_2_rows[10][1];
+            }
+
+
+            if ($contents[0][0] == 'STEPS')
+            {
+                $steps_rows = array_slice($contents, 2);
+                $steps = [];
+                for ($ct = 0; $ct < count($steps_rows); $ct++)
+                {
+                    if ($ct < count($steps_rows))
+                    {
+                        $steps_data['step'] = $steps_rows[$ct][0];
+                        $steps_data['duration'] = $steps_rows[$ct][1];
+                        $steps_data['teacher_activity'] = $steps_rows[$ct][2];
+                        $steps_data['student_activity'] = $steps_rows[$ct][3];
+                        $steps_data['knowledge'] = $steps_rows[$ct][4];
+                        $steps_data['skills'] = $steps_rows[$ct][5];
+                        $steps_data['values'] = $steps_rows[$ct][6];
+                        $steps_data['output'] = $steps_rows[$ct][7];
+                        $steps_data['assessment_criteria'] = $steps_rows[$ct][8];
+                    }
+                    $steps[] = $steps_data;
+                }
+            }
+        }
+
+        $lp_info_array = array_merge($info_1, $info_2);
+        $lp_info_array['owner'] = intval(request()->teacher);
+        $lp_info_array['subject'] = intval(request()->subject);
+        $lp_info_array['learners_no'] = $info_1['male_learners'] + $info_1['female_learners'];
+
+        $lp_rules = [
+            'topic' => 'required',
+            'subject' => 'required',
+            'class' => 'required',
+            'learners_no' => 'required',
+            'term' => 'required',
+            'theme' => 'required',
+            'competency' => 'required',
+            'learning_outcomes' => 'required',
+            'generic_skills' => 'required',
+            'values' => 'required',
+            'cross_cutting_issues' => 'required',
+            'key_learning_outcomes' => 'required',
+            'pre_requisite_knowledge' => 'required',
+            'learning_materials' => 'required',
+            'learning_methods' => 'required',
+            'references' => 'required',
+            'activity_aim' => 'required',
+        ];
+
+        $validate_lp = Validator::make($lp_info_array, $lp_rules);
+
+        if ($validate_lp->fails()) {
+            // Validation failed
+            $errors = $validate_lp->errors();
+            return back()
+                    ->with('error', $errors);
+        }
+
+        $school = User::find(request()->teacher);
+
+        $lp_info_array['school'] = intval($school->school);
+        $lp_info_array['created_by'] = auth()->user()->id;
+        $lesson = LessonPlan::create($lp_info_array);
+
+        if($steps)
+        {
+            foreach ($steps as $step)
+            {
+                $step_rules = [
+                    'step' => 'required',
+                ];
+
+                $validate_step = Validator::make($step, $step_rules);
+
+                if ($validate_step->fails()) {
+                    // Validation failed
+                    $errors = $validate_step->errors();
+                    return redirect()
+                        ->back()
+                        ->with('error', $errors);
+                }
+
+                $step['lesson_plan'] = $lesson->id;
+                $step['created_by'] = auth()->user()->id;
+
+                LessonStep::create($step);
+            }
+        }
+
+        $log['message'] = 'Lessonplan was uploaded and created with id '.$lesson->id;
         $log['user_id'] = Auth()->user()->id;
         $log['action'] = 'Upload Lessonplan';
         $log['ip_address'] = request()->ip();
@@ -218,8 +418,9 @@ class LessonPlanController extends Controller
         Logs::create($log);
 
         return redirect()
-            ->route('lesson.plans')
-            ->with('status', 'The Lesson Plan has been uploaded successfully.');
+                ->route('get.lesson.plan', $lesson->id)
+                ->with('status', 'Lesson plan created successfully.');
+        // return response()->json($steps);
     }
 
     public function deleteLessonPlan()
@@ -695,7 +896,12 @@ class LessonPlanController extends Controller
             // Add the school name to the lessonPlan object
             $schoolId = $lessonPlan->school;
             $school = School::find($schoolId);
-            $lessonPlan->school = $school->id ? $school->name : 'N/A';
+            // $lessonPlan->school = $school->id ? $school->name : 'N/A';
+            if ($owner) {
+                $lessonPlan->owner = $owner->id ? $owner->name : 'N/A';
+            } else {
+                $lessonPlan->owner = 'N/A';
+            }
 
             // Get the duration from the steps
             $lessonPlan->duration = LessonStep::where(['lesson_plan' => $lessonPlan->id])->sum('duration');
--- resources/views/lesson-plan/upload.blade.php
+++ resources/views/lesson-plan/upload.blade.php
@@ -1,3 +1,20 @@
+<style>
+
+    .select2-container--default .select2-selection--single {
+        padding: 4px;
+        border: 1px solid #d2d6da !important;
+    }
+
+    .select2-container .select2-selection--single {
+        height: 38px !important;
+    }
+
+    .select2-container--default .select2-selection--single .select2-selection__rendered {
+        color: #7b809a !important;
+        font-size: 0.875rem !important;
+        font-weight: 400 !important;
+    }
+</style>
 <x-layout bodyClass="g-sidenav-show bg-gray-200">
 
     <x-navbars.sidebar activePage="lesson-plans"></x-navbars.sidebar>
@@ -10,7 +27,7 @@
                 <div class="card card-plain h-100">
                     <div class="card-body p-3">
                         <div class=" me-3 my-3 text-end">
-                            <a class="btn bg-gradient-info mb-0" href="{{ asset('assets') }}/download/act_now_lesson_plan.xlsx">
+                            <a class="btn bg-gradient-info mb-0" href="{{ asset('assets') }}/download/act_now_lp_template.docx">
                                 <i class="material-icons text-sm">download</i>&nbsp;&nbsp;Download Example LP
                             </a>
                         </div>
@@ -43,7 +60,7 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-9 d-flex">
+                                        {{-- <div class="col-md-9 d-flex">
                                             <ol>
                                                 <li>First, download the upload example file using the blue button above.</li>
                                                 <li>Edit the file on your computer /tablet /phone and save it.</li>
@@ -56,6 +73,24 @@
                                                     </ul>
                                                 </li>
                                                 <li>The steps on Sheet2 should be on seperate rows</li>
+                                            </ol>
+                                        </div> --}}
+                                        <div class="col-md-9 d-flex">
+                                            <ol>
+                                                <li>First, download the upload example file using the blue button above.</li>
+                                                <li>Edit the file on your computer /tablet /phone and save it.</li>
+                                                <li>The file has 2 tables i.e. <strong>Table 1 is for Preliminary Information</strong> and <strong>Table 2 for steps</strong></li>
+                                                <li>The class cell<strong>MUST</strong> be in the form of <strong>S1, S2, S3, S4, S5, S6</strong>. For example S1 is Senior 1</li>
+                                                <li>The term cell <strong>MUST</strong> be in the form of <strong>1, 2, 3</strong>. For example 1 is Term 1</li>
+                                                <li>Demo data has been added and it sould be follwoed the way it is input.</li>
+                                                <li>You can add another row for a new step on the Steps table, and you can add as many they are available for your lesson.</li>
+                                                <li>Upload the saved file in the upload section below:
+                                                    <ul>
+                                                        <li>Make sure you select the subject for the lesson plan.</li>
+                                                        <li>Click on the choose file, and select the file you saved.</li>
+                                                        <li>Then click on the upload button.</li>
+                                                    </ul>
+                                                </li>
                                             </ol>
                                         </div>
                                         <div class="card ">
@@ -76,16 +111,17 @@
 
                                                     <p class="card-text mb-2 font-weight-bold">Select Subject for the Lesson Plan</p>
                                                     <div class="mb-3 col-md-6">
-                                                        <select class="form-select border-2 p-2" name="subject" aria-label="">
+                                                        <select id="subject-records" class="form-select border-2 p-2" name="subject" aria-label="">
                                                             <option value="" selected>Select Subject</option>
                                                             @foreach($subjects as $subject)
                                                             <option value="{!! $subject->id !!}">{!! $subject->name !!}</option>
                                                             @endforeach
                                                         </select>
                                                     </div>
-                                                    <p class="card-text mt-3 mb-2"><strong>Select File to Upload</strong> - <small class="text-muted">{{__('Please upload only Excel (.xlsx or .xls) files')}}</small></p>
+                                                    <p class="card-text mt-3 mb-2"><strong>Select File to Upload</strong> - <small class="text-muted">{{__('Please upload only Microsoft Word (.docx or .doc) files')}}</small></p>
                                                     {{-- <input type="file" id="lpFile" name="lesson_plan_file" accept=".doc,.docx"> --}}
-                                                    <input type="file" id="lpFile" name="lesson_plan_file" accept=".xls,.xlsx">
+                                                    {{-- <input type="file" id="lpFile" name="lesson_plan_file" accept=".xls,.xlsx"> --}}
+                                                    <input type="file" id="lpFile" name="lesson_plan_file" accept=".doc,.docx">
                                                     <button type="submit" class="btn btn-success mt-4">Upload Lesson Plan</button>
                                                 </form>
                                         </div>
@@ -103,5 +139,6 @@
 <script>
     $(document).ready(function() {
         $('#teacher-records').select2();
+        $('#subject-records').select2();
     });
 </script>
